6c1a8bf690d3cde46a3f356708256e3fd72a43fe
Benedikt Schmied <github-ce@benedikt-schmied.de>
2018-10-21 13:02:40 +0200

    new projects which treats the 'kv_language'

---
+++ kv_language_and_pyinstaller/auxiliary1/auxiliary1_1/miniprinter.py
@@ -0,0 +1,21 @@
+'''
+Created on 16.10.2018
+
+@author: schmied
+'''
+
+class c_miniprinter(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+    
+    def run(self):
+        print("... running the miniprinter... ")
+        
--- pyinstaller.sh
+++ pyinstaller.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-pyinstaller.exe -y packages_and_pyinstaller.none_bundled.spec
-pyinstaller.exe -y packages_and_pyinstaller.bundled.spec
+#pyinstaller.exe -y packages_and_pyinstaller.none_bundled.spec
+#pyinstaller.exe -y packages_and_pyinstaller.bundled.spec
 
 #pyinstaller.exe -y kivy_and_pyinstaller.none_bundled.spec
+#pyinstaller.exe -y kivy_and_pyinstaller.bundled.spec
+
+pyinstaller.exe -y kv_language_and_pyinstaller.none_bundled.spec
 #pyinstaller.exe -y kivy_and_pyinstaller.bundled.spec
